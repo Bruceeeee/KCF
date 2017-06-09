81bfb10ebc66b81408a509328a22547a167464d9
Bruceeeee <zhanghtzj@163.com>
2017-06-09 16:09:50 +0800

    Create HOG.py

---
+++ HOG.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+from numpy.linalg import norm
+
+def hog(img, bin_n=8, cell_size=4):
+    img = cv2.resize(img,(128,128))
+    gx = cv2.Sobel(img, cv2.CV_32F, 1, 0)
+    gy = cv2.Sobel(img, cv2.CV_32F, 0, 1)
+    mag, ang = cv2.cartToPolar(gx, gy)
+    bin = np.int32(bin_n*ang/(2*np.pi))
+
+    bin_cells = []
+    mag_cells = []
+
+    cellx = celly = cell_size
+
+    for i in range(0,img.shape[0]/celly):
+        for j in range(0,img.shape[1]/cellx):
+            bin_cells.append(bin[i*celly : i*celly+celly, j*cellx : j*cellx+cellx])
+            mag_cells.append(mag[i*celly : i*celly+celly, j*cellx : j*cellx+cellx])   
+
+    hists = [np.bincount(b.ravel(), m.ravel(), bin_n) for b, m in zip(bin_cells, mag_cells)]
+    hist = np.hstack(hists)
+
+    # transform to Hellinger kernel
+    eps = 1e-7
+    hist /= hist.sum() + eps
+    hist = np.sqrt(hist)
+    hist /= norm(hist) + eps
+    hist_out = np.reshape(hist,(32,32,8))
+
+
+    return hist_out
